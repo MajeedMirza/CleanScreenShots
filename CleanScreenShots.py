d1964dc2cbbc3d1907e94fbaf629d71005f23bf1
Majeed Mirza <MajeedMirza@users.noreply.github.com>
2018-07-12 23:49:20 -0400

    Create CleanScreenShots.py

---
+++ CleanScreenShots.py
@@ -0,0 +1,8 @@
+import os
+
+DESKTOP = 'PUT DESKTOP PATH HERE'
+SSHOT = DESKTOP + 'ScreenShots/' #Create a folder called "ScreenShots" on Desktop before use
+for fname in os.listdir(DESKTOP):
+    if "Screen Shot" in fname and ".png" in fname:
+       os.rename(DESKTOP + fname, SSHOT + fname)
+       print "Moved " + fname + " to " + SSHOT
